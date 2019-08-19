d15006fdc2372e2ca2f25af4ecdc7fa304819a16
nidhi2611 <nidhi.gupta_cs18@gla.ac.in>
2019-08-19 14:48:27 +0530

    initial commit

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -1,10 +1,13 @@
 package question2;
 
+import java.util.Scanner;
 public class Question2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name1 = sc.nextLine().trim();
         String name2 = sc.nextLine().trim();
+        System.out.println(name1 + "University" + name2);
+        sc.close();
 
 
 
---
+++ src/question3/Question2.java
@@ -0,0 +1,5 @@
+package question3;
+
+public class Question3 {
+    public static void main(String[] args)
+}
