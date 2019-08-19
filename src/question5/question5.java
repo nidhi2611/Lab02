65c3938e661b1bc2c3b5b8505e82398b69753ec5
nidhi2611 <nidhi.gupta_cs18@gla.ac.in>
2019-08-19 15:21:33 +0530

    final commit

--- src/question5/question5.java
+++ src/question5/question5.java
@@ -6,4 +6,8 @@ public class question5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        if (n % 2 == 0)
+            System.out.print("Even number");
+        else
+            System.out.println("Odd number");
     }
