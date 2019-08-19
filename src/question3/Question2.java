d5b1c6f211634f7a0270cfae58c510d07f221ef5
nidhi2611 <nidhi.gupta_cs18@gla.ac.in>
2019-08-19 15:07:54 +0530

    initial commit

--- src/question3/Question2.java
+++ src/question3/Question2.java
@@ -1,12 +1,20 @@
 package question3;
 
 import java.util.Scanner;
-public class Question3 {
-    public static void main(String[] args)
 
-    Scanner sc = new Scanner(System.in);
-    int num1 = sc.nextInt();
-    int num2 = sc.nextInt();
+public class Question2 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(num1 + "+" + num2 + "=" + (num1 + num2));
+        System.out.println(num1 + "-" + num2 + "=" + (num1 - num2));
+        System.out.println(num1 + "*" + num2 + "=" + (num1 * num2));
+        System.out.println(num1 + "/" + num2 + "=" + (num1 / num2));
+        System.out.println(num1 + "%" + num2 + "=" + (num1 % num2));
+        sc.close();
+    }
 
 }
