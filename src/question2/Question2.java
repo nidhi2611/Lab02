381e19bff9d09769eab33e14bd57202d83e27428
nidhi2611 <nidhi.gupta_cs18@gla.ac.in>
2019-08-19 14:40:52 +0530

    input taken

--- src/question2/Question2.java
+++ src/question2/Question2.java
@@ -3,6 +3,9 @@ package question2;
 public class Question2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String name1 = sc.nextLine().trim();
+        String name2 = sc.nextLine().trim();
+
 
 
     }
