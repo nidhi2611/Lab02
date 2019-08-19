be6ef5de28dbb5a88065be46a4fa8ead838d7125
nidhi2611 <nidhi.gupta_cs18@gla.ac.in>
2019-08-19 15:11:38 +0530

    final commit

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -5,6 +5,12 @@ public class Question4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        if (n > 0)
+            System.out.println("Positive");
+        else if (n < 0)
+            System.out.println("Negative");
+        else if (n == 0)
+            System.out.println("Zero");
 
     }
 }
