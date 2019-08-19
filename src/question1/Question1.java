a926bc83133d83f8022927c07181b89f1524fbe3
nidhi2611 <nidhi.gupta_cs18@gla.ac.in>
2019-08-19 14:38:32 +0530

    main created

--- src/question1/Question1.java
+++ src/question1/Question1.java
@@ -5,7 +5,7 @@ public class Question1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String userName = sc.nextLine().trim();
-
+        System.out.println("Welcome, " + userName + "!");
 
 
 
---
+++ src/question2/Question2.java
@@ -0,0 +1,9 @@
+package question2;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
