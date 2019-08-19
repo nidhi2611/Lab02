0bec0c7f691597529cd716d777e66d0d0369e598
nidhi2611 <nidhi.gupta_cs18@gla.ac.in>
2019-08-19 14:50:59 +0530

    inputted integers

--- src/question3/Question2.java
+++ src/question3/Question2.java
@@ -1,5 +1,12 @@
 package question3;
 
+import java.util.Scanner;
 public class Question3 {
     public static void main(String[] args)
+
+    Scanner sc = new Scanner(System.in);
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
+
+
 }
