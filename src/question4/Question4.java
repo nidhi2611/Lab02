a494fee654d3aacac7e0b00bb3084d9d6be024d1
nidhi2611 <nidhi.gupta_cs18@gla.ac.in>
2019-08-19 15:09:38 +0530

    number inputted

--- src/question4/Question4.java
+++ src/question4/Question4.java
@@ -1,7 +1,10 @@
 package question4;
 
+import java.util.Scanner;
 public class Question4 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
     }
 }
