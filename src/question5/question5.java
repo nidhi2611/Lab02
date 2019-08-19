da31cca49900888cbc1ad63dd2f7e8100b49ff7e
nidhi2611 <nidhi.gupta_cs18@gla.ac.in>
2019-08-19 15:20:15 +0530

    initial commit

---
+++ src/question5/question5.java
@@ -0,0 +1,9 @@
+package question5;
+
+import java.util.Scanner;
+
+public class question5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
